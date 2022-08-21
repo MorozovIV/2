b299bc3991fa563a02d858d7af97ebb14fbcc678
Shadow <Shadow-zy@yandex.ru>
2022-08-21 17:24:45 +0300

    2

--- socet/server1.py
+++ socet/server1.py
@@ -12,9 +12,15 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
+        if self.data == b'1':
+            print('+')
+        elif self.data == b'2':
+            print('++')
+        else:
+            print('-')
+
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
-
 if __name__ == "__main__":
     HOST, PORT = "10.0.10.200", 1234
 
