799e720e0b1e1e05f42babe9865c2a2999ec8aad
Shadow <Shadow-zy@yandex.ru>
2022-08-21 14:28:14 +0300

    2

--- socet/server0.py
+++ socet/server0.py
@@ -2,7 +2,7 @@
 from socket import *
 s = socket(AF_INET,SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-s.bind(('127.0.0.1',1234))
+s.bind(('10.0.10.200',1234))
 s.listen(5)
 while 1 :
     print('server start')
