3015dd8e79dfe73bbf04c641f19c500bc002925d
Shadow <Shadow-zy@yandex.ru>
2022-08-22 23:37:52 +0300

    2

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-x = 100
-if x% 3:
-    print('1')
-elif x%2:
-    print('2')
-elif x%5:
-    print('3')
-elif x%50:
-    print('4')
-else:
-    print('n')
+import time
 
-x =10
-while 1:
-    if x%2:
-        print(x)
-    x -=1
+for t in range(5, 0, -1):
+    print(t, end=' ')
+    time.sleep(1)
